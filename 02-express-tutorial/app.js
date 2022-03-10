4c7333f7199431ced0ffec98e92f94206b414a7c
abbabashir <abbabashir516@gmail.com>
2022-03-10 09:57:32 +0100

    first commit

---
+++ 02-express-tutorial/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+let {people} = require('./data')
+
+app.use(express.static('./methods-public'))
+app.use(express.urlencoded({extended: false}))
+
+app.get('/api/people',(req,res) => {
+    res.status(200).json({success:true,data:people})
+})
+
+app.post('/login',(req,res) => {
+    res.send("POSTING YEAH")
+})
+
+app.listen(5000, () => {
+    console.log('server listening on port 5000');
+})
